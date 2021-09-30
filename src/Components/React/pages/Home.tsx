af1dfd1ac661b01fb48e4c5b8c0b9616dec2434f
Riccardo Cafagna <riccafagna@gmail.com>
2021-09-30 20:34:12 -0300

    Home style

--- src/Components/React/pages/Home.tsx
+++ src/Components/React/pages/Home.tsx
@@ -7,17 +7,17 @@ export default function Home() {
     return (
         <div className="Container">
             <div className="Button-container">
-                <button className="Button-pick">
+                <button className="Button-pick cyber">
                     cyber security
                 </button>
-                <button className="Button-pick">
+                <button className="Button-pick npm">
                     npm vulnerabilities
                 </button>
             </div>
-            <div>
-                <h2>Do you have any key? (optional)</h2>
-                <input placeholder="your key"></input>
-                <a>start cyber form -</a>
+            <div className="Container-Body">
+                <h2 className="container-title">Do you have any key? (optional)</h2>
+                <input placeholder="your key" className="key-input"></input>
+                <a className="start-link">start cyber form -{">"}</a>
             </div>
         </div>
     )
